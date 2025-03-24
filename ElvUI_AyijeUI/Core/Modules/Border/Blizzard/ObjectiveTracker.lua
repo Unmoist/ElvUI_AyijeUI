fd7480083de6041f8ba24b0d82f5ccc1b25ebaba
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-03-24 12:27:42 +0100

    teet

--- ElvUI_AyijeUI/Core/Modules/Border/Blizzard/ObjectiveTracker.lua
+++ ElvUI_AyijeUI/Core/Modules/Border/Blizzard/ObjectiveTracker.lua
@@ -99,7 +99,7 @@ end
 function S:ReskinOjectiveTrackerHeader(header)
 	if not header or not header.Text then return end
 
-	header.Text:SetFont(E.db.general.fonts.objective.font, E.db.general.fonts.objective.size, E.db.general.fonts.objective.outline)
+	header.Text:SetFont(Engine.Font, E.db.general.fonts.objective.size, E.db.general.fonts.objective.outline)
 end
 
 function S:Blizzard_ObjectiveTracker()
