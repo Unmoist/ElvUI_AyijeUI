3267c5b7ee6bfc329ca9ecb5b7b1d0959e466a1b
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-03-25 10:18:05 +0100

    This time for sure.

--- ElvUI_AyijeUI/Core/Modules/Border/Addons/OmniCD.lua
+++ ElvUI_AyijeUI/Core/Modules/Border/Addons/OmniCD.lua
@@ -19,9 +19,31 @@ function S:OmniCD_ConfigGUI()
 	end)
 end
 
-function S:OmniCD_Party_Icon()
+function S:OmniCD_Party_Icons()
 	local OmniCD = _G.OmniCD and _G.OmniCD[1]
 	if not OmniCD then return end
+
+	hooksecurefunc(OmniCD.Party.BarFrameIconMixin, "SetExIconName", function(icon)
+		if not icon or not icon.GetObjectType then return end
+	
+		if icon:GetObjectType() == "Texture" then
+				icon = icon:GetParent()
+		end
+	
+		if icon.border then
+				icon.border:Kill()
+		end
+	
+		local border = CreateFrame("Frame", nil, icon, BackdropTemplateMixin and "BackdropTemplate")
+		if not border then return end
+	
+		border:SetBackdrop(Private.Border)
+		border:SetFrameLevel(icon:GetFrameLevel() + 2)
+		border:SetPoint("TOPLEFT" , icon, "TOPLEFT", -7.8, 7.8)
+		border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 7.8, -7.8)
+		
+		icon.border = border
+	end)
 
 	hooksecurefunc(OmniCD.Party.BarFrameIconMixin, "SetBorder", function(icon)
 		if not icon or not icon.GetObjectType then return end
@@ -37,28 +59,12 @@ function S:OmniCD_Party_Icon()
 		local border = CreateFrame("Frame", nil, icon, BackdropTemplateMixin and "BackdropTemplate")
 		if not border then return end
 
-		local width = icon:GetWidth() + 16
-		local height = icon:GetHeight() + 16
-		local x, y = icon:GetCenter()
-
-		border:SetFrameStrata(icon:GetFrameStrata())
-		border:SetFrameLevel(icon:GetFrameLevel() + 3)
-		border:SetBackdrop(Engine.Border)
-		border:SetSize(width, height)
-
-		if x and y then
-				border:ClearAllPoints()
-				border:SetPoint("CENTER", UIParent, "BOTTOMLEFT", math.floor(x), math.floor(y + 0.7))
-		end
-
+		border:SetBackdrop(Private.Border)
+		border:SetFrameLevel(icon:GetFrameLevel() + 2)
+		border:SetPoint("TOPLEFT" , icon, "TOPLEFT", -8, 8)
+		border:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 8, -8)
+		
 		icon.border = border
-	end)
-
-	hooksecurefunc(OmniCD.Party.BarFrameIconMixin, "SetExIconName", function(icon)
-			if icon and not icon.Border then
-					BORDER:CreateBorder(icon)
-					icon.Border = true
-			end
 	end)
 end
 
