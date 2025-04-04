7ab6964a97689980f57883bdc1e7c65619ef9ce8
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-04-04 09:09:29 +0200

    castbar fux

--- ElvUI_AyijeUI/Core/Modules/Border/ElvUI/CastBars.lua
+++ ElvUI_AyijeUI/Core/Modules/Border/ElvUI/CastBars.lua
@@ -5,12 +5,17 @@ local BORDER = E:GetModule('BORDER')
 local S = E:GetModule('Skins')
 local UF = E:GetModule("UnitFrames")
 
+local blizzcastback = "Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcastback.tga"
+local blizzcast = "Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcast.tga"
+local blizzcastchannel = "Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcastchannel.tga"
+local blizzcastnonbreakable = "Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcastnonbreakable.tga"
+
 function S:ElvUI_UnitFrames_SkinCastBar(_, frame)
 	if not frame.Castbar then	return end
 
 	if not frame.TEXTURESET then
-		frame.Castbar:SetStatusBarTexture("Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcast.tga")
-		frame.Castbar.bg:SetTexture("Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcastback.tga")
+		frame.Castbar:SetStatusBarTexture(blizzcast)
+		frame.Castbar.bg:SetTexture(blizzcastback)
 		frame.TEXTURESET = true
 	end
 	
@@ -21,19 +26,19 @@ function S:ElvUI_UnitFrames_SkinCastBar(_, frame)
 
 	frame.Castbar:HookScript("OnValueChanged", function(self)
 		if self.channeling then
-			self:SetStatusBarTexture("Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcastchannel.tga")
+			self:SetStatusBarTexture(blizzcastchannel)
 			self:SetStatusBarColor(1, 1, 1, 1)
 			self.bg:SetVertexColor(1, 1, 1)
 		elseif not self.notInterruptible and self.unit and self.unit ~= "player" then
-			self:SetStatusBarTexture("Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcast.tga")
+			self:SetStatusBarTexture(blizzcast)
 			self:SetStatusBarColor(1, 1, 1, 1)
 			self.bg:SetVertexColor(1, 1, 1)
 		elseif self.notInterruptible then
-			self:SetStatusBarTexture("Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcastnonbreakable.tga")
+			self:SetStatusBarTexture(blizzcastnonbreakable)
 			self:SetStatusBarColor(1, 1, 1, 1)
 			self.bg:SetVertexColor(1, 1, 1)
 		else
-			self:SetStatusBarTexture("Interface\\AddOns\\ElvUI_AyijeUI\\Media\\Statusbar\\blizzcast.tga")
+			self:SetStatusBarTexture(blizzcast)
 			self:SetStatusBarColor(1, 1, 1, 1)
 			self.bg:SetVertexColor(1, 1, 1) 
 		end
