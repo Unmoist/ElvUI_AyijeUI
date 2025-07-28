0a4574db9621955e302914b14f70b8dfbf0a7df1
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-07-28 14:34:14 +0200

    new grid2

--- ElvUI_AyijeUI/Core/Modules/Border/Addons/Grid2.lua
+++ ElvUI_AyijeUI/Core/Modules/Border/Addons/Grid2.lua
@@ -12,21 +12,72 @@ if E.db.forceSeparators == nil then
 end
 
 local function GetUnitFrame()
-    for i = 1, 2 do
-        for k = 2, 5 do
-            local unitName = "Grid2LayoutHeader" .. i .. "UnitButton" .. k
-            local unitButton = _G[unitName]
-            if unitButton then 
-                if not IsInRaid() or E.db.forceSeparators then
-                    BORDER:CreateSeparator(unitButton)
-                    unitButton.separator:Show()
-                    unitButton.separator:SetPoint("TOPRIGHT", unitButton, 0, 3)
-                elseif unitButton.separator then
-                    unitButton.separator:Hide()
+    local spec = GetSpecialization()
+    local specID = GetSpecializationInfo(spec)
+    local healerSpecs = {
+        [105] = true, -- Restoration(Druid)
+        [270] = true, -- Mistweaver(Monk)
+        [65]  = true, -- Holy(Paladin)
+        [257] = true, -- Holy(Priest)
+        [256] = true, -- Discipline(Priest)
+        [264] = true, -- Restoration(Shaman)
+        [1468] = true, -- Preservation(Evoker)
+    }
+
+    if healerSpecs[specID] then
+        for i = 1, 2 do
+            for k = 1, 5 do -- Start at 1 now
+                local unitName = "Grid2LayoutHeader" .. i .. "UnitButton" .. k
+                local unitButton = _G[unitName]
+
+                BORDER:CreateBorder(_G.Grid2LayoutFrame)
+
+                if unitButton then 
+                    -- Always try to remove border
+                    if unitButton.border then 
+                        unitButton.border:Hide()
+                        unitButton.border = nil
+                        unitButton.IsBorder = false
+                    end
+
+                    -- Only affect separator if k > 1
+                    if k > 1 then
+                        if not IsInRaid() or E.db.forceSeparators then
+                            BORDER:CreateSeparator(unitButton)
+                            unitButton.separator:Show()
+                            unitButton.separator:SetPoint("TOPRIGHT", unitButton, 0, 3)
+                        elseif unitButton.separator then
+                            unitButton.separator:Hide()
+                        end
+                    end
                 end
             end
         end
-    end
+
+    else
+        for o = 1, 2 do
+            for u = 1, 5 do
+                local unitName = "Grid2LayoutHeader" .. o .. "UnitButton" .. u
+                local unitButton = _G[unitName]
+
+                if unitButton then
+                    if unitButton.separator then
+                        unitButton.separator:Hide()
+                        unitButton.separator = nil
+                    end
+                if _G.Grid2LayoutFrame.border then
+                    _G.Grid2LayoutFrame.border:Hide()
+                    _G.Grid2LayoutFrame.border:ClearAllPoints()
+                    _G.Grid2LayoutFrame.border:SetParent(nil)
+                    _G.Grid2LayoutFrame.border = nil
+                    _G.Grid2LayoutFrame.IsBorder = false
+                end
+                    
+                    BORDER:CreateBorder(unitButton)
+                end
+            end
+        end
+    end 
 end
 
 -- Slash command
@@ -46,14 +97,19 @@ SlashCmdList["GRID2SEPARATOR"] = function(msg)
     end
 end
 
+local function Grid2GetUnitFrame()
+    GetUnitFrame()
+
+end
+
 function S:Grid2()
     if not E.db.AYIJE.skins.grid2 then return end
+
     local Grid2Layout = Grid2:GetModule("Grid2Layout")
     local Grid2 = Grid2
 
-    BORDER:CreateBorder(_G.Grid2LayoutFrame)
-
-    hooksecurefunc(Grid2Layout, "LoadLayout", GetUnitFrame)
+    hooksecurefunc(Grid2Layout, "LoadLayout", Grid2GetUnitFrame)
+	S:RegisterEvent("PLAYER_TALENT_UPDATE", Grid2GetUnitFrame)
 
     -- Reminder after /reload if separators are forced on
     E:Delay(1, function()
