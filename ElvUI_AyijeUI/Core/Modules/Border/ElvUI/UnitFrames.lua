e5cd7770f9252d5f734dc614421023de9424696e
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-07-31 08:45:00 +0200

    better unitframe border

--- ElvUI_AyijeUI/Core/Modules/Border/ElvUI/UnitFrames.lua
+++ ElvUI_AyijeUI/Core/Modules/Border/ElvUI/UnitFrames.lua
@@ -5,211 +5,143 @@ local BORDER = E:GetModule('BORDER')
 local S = E:GetModule('Skins')
 local UF = E:GetModule("UnitFrames")
 local GetTime = GetTime
+local gsub, strmatch = gsub, strmatch
 
 local function BorderAndSeparator(f)
     if not f then return end
-    
-    -- Cache border states to prevent redundant updates
+
     local hasBorder = f.border and f.border:IsShown()
-    local hasHealthBorder = f.Health.border and f.Health.border:IsShown()
-    local hasPowerBorder = f.Power.border and f.Power.border:IsShown()
-    local hasPowerSeparator = f.Power.separator and f.Power.separator:IsShown()
+    local hasHealthBorder = f.Health and f.Health.border and f.Health.border:IsShown()
+    local hasPowerBorder = f.Power and f.Power.border and f.Power.border:IsShown()
+    local hasPowerSeparator = f.Power and f.Power.separator and f.Power.separator:IsShown()
 
     if f.POWERBAR_DETACHED or f.USE_POWERBAR_OFFSET then
-        if hasBorder then
-            f.border:Hide()
-        end
+        if hasBorder then f.border:Hide() end
 
-        if not f.Health.backdrop.border then
+        if f.Health and f.Health.backdrop and not f.Health.backdrop.border then
             BORDER:CreateBorder(f.Health.backdrop)
         end
     else
-        if hasHealthBorder then
-            f.Health.border:Hide()
-        end
+        if hasHealthBorder then f.Health.border:Hide() end
 
         if not hasBorder then
             BORDER:CreateBorder(f, 15)
         end
     end
 
-    if f.USE_POWERBAR and (hasBorder or f.Health.backdrop.border) then
+    if f.USE_POWERBAR and (hasBorder or (f.Health and f.Health.backdrop and f.Health.backdrop.border)) then
         if f.POWERBAR_DETACHED or f.USE_POWERBAR_OFFSET then
-            if not hasPowerBorder then
+            if f.Power and f.Power.backdrop and not hasPowerBorder then
                 BORDER:CreateBorder(f.Power.backdrop)
             end
-            if hasPowerSeparator then
-                f.Power.separator:Hide()
-            end
+            if hasPowerSeparator then f.Power.separator:Hide() end
         else
-            if not hasPowerSeparator then
+            if f.Power and not hasPowerSeparator then
                 BORDER:CreateSeparator(f.Power)
             end
-            if hasPowerBorder then
-                f.Power.border:Hide()
-            end
+            if hasPowerBorder then f.Power.border:Hide() end
         end
     end
 end
 
 function S:ElvUI_UnitFrames(_, unit)
     local frameName = gsub(E:StringTitle(unit), 't(arget)', 'T%1')
-    local frame = UF[unit]
     local enabled = UF.db.units[unit].enable
 
-    -- Player
-    if frameName == 'Player' and enabled then
-        if E.db.AYIJE.border.Player then
-            local PF = UF['player']
-            BorderAndSeparator(PF)
-            if E.db.AYIJE.border.Playersep then
-                if PF.Power.separator then
-                    PF.Power.separator:Show()
-                    PF.Power.separator:SetIgnoreParentAlpha(false)
-                end
-            else
-                if PF.Power.separator then
-                    PF.Power.separator:Hide()
-                end
+    local config = E.db.AYIJE and E.db.AYIJE.border
+    if not config then return end
+
+    local f = UF[unit]
+    if not f then return end
+
+    if frameName == 'Player' and enabled and config.Player then
+        BorderAndSeparator(f)
+        if config.Playersep and f.Power and f.Power.separator then
+            f.Power.separator:Show()
+            f.Power.separator:SetIgnoreParentAlpha(false)
+        elseif f.Power and f.Power.separator then
+            f.Power.separator:Hide()
+        end
+    elseif frameName == 'Target' and enabled and config.Target then
+        BorderAndSeparator(f)
+        if config.Targetsep and f.Power and f.Power.separator then
+            f.Power.separator:Show()
+            f.Power.separator:SetIgnoreParentAlpha(false)
+        elseif f.Power and f.Power.separator then
+            f.Power.separator:Hide()
+        end
+    elseif frameName == 'TargetTarget' and enabled and config.TargetofTarget then
+        BorderAndSeparator(f)
+    elseif frameName == 'Focus' and enabled and config.Focus then
+        BorderAndSeparator(f)
+    elseif frameName == 'FocusTarget' and enabled and config.FocusTarget then
+        BorderAndSeparator(f)
+    elseif frameName == 'Pet' and enabled and config.Pet then
+        BorderAndSeparator(f)
+    elseif frameName == 'PetTarget' and enabled and config.PetTarget then
+        BorderAndSeparator(f)
+    elseif frameName == 'TargetTargetTarget' and enabled and config.TargetofTargetofTarget then
+        BorderAndSeparator(f)
+    end
+end
+
+function S:ElvUI_UnitFramesGroup(_, group, numGroup)
+    local config = E.db.AYIJE and E.db.AYIJE.border
+    if not config then return end
+
+    for i = 1, numGroup do
+        local unit = group .. i
+        local enabled = UF.db.units[group].enable
+
+        if unit == "boss"..i and enabled and config.Boss and E.db.unitframe.units.boss.enable then
+            local BF = _G["ElvUF_Boss"..i]
+            BorderAndSeparator(BF)
+            if BF.Power and BF.Power.separator then
+                BF.Power.separator:SetIgnoreParentAlpha(false)
+            end
+            if BF.border then
+                BF.border:SetFrameLevel(22)
             end
         end
-    end
 
-    -- Target
-    if frameName == 'Target' and enabled then
-        if E.db.AYIJE.border.Target then
-            local TF = UF['target']
-            BorderAndSeparator(TF)
-            if E.db.AYIJE.border.Targetsep then
-                if TF.Power.separator then
-                    TF.Power.separator:Show()
-                    TF.Power.separator:SetIgnoreParentAlpha(false)
-                end
-            else
-                if TF.Power.separator then
-                    TF.Power.separator:Hide()
-                end
-            end
-        end
-    end
-
-    -- Target of Target
-    if frameName == 'TargetTarget' and enabled then
-        if E.db.AYIJE.border.TargetofTarget then
-            local TTF = UF['targettarget']
-            BorderAndSeparator(TTF)
-        end
-    end
-
-    -- Focus
-    if frameName == 'Focus' and enabled then
-        if E.db.AYIJE.border.Focus then
-            local FF = UF['focus']
-            BorderAndSeparator(FF)
-        end
-    end
-
-    -- Focus Target
-    if frameName == 'FocusTarget' and enabled then
-        if E.db.AYIJE.border.FocusTarget then
-            local FTF = UF['focustarget']
-            BorderAndSeparator(FTF)
-        end
-    end
-
-    -- Pet
-    if frameName == 'Pet' and enabled then
-        if E.db.AYIJE.border.Pet then
-            local PetFrame = UF['pet']
-            BorderAndSeparator(PetFrame)
-        end
-    end
-
-    -- Pet Target
-    if frameName == 'PetTarget' and enabled then
-        if E.db.AYIJE.border.PetTarget then
-            local PettargetFrame = UF['pettarget']
-            BorderAndSeparator(PettargetFrame)
-        end
-    end
-
-    -- Target of Target of Target
-    if frameName == 'TargetTargetTarget' and enabled then
-        if E.db.AYIJE.border.TargetofTargetofTarget  then
-            local TTTF = UF['targettargettarget']
-            BorderAndSeparator(TTTF)
+        if unit == "arena"..i and enabled and config.Arena and E.db.unitframe.units.arena.enable then
+            local AF = _G["ElvUF_Arena"..i]
+            BorderAndSeparator(AF)
         end
     end
 end
 
-function S:ElvUI_UnitFramesGroup (_, group, numGroup)
-	for i = 1, numGroup do
-		local unit = group..i
-		local frame = UF[unit]
-		local enabled = UF.db.units[group].enable
+function S:ElvUI_UnitFramesGroupRaidParty(_, group)
+    local db = UF.db.units[group]
+    local config = E.db.AYIJE and E.db.AYIJE.border
+    if not config then return end
 
-        if unit == "boss"..i and enabled then
-            if E.db.AYIJE.border.Boss and E.db.unitframe.units.boss.enable then
-                local BF = _G["ElvUF_Boss"..i]
-                BorderAndSeparator(BF)
-                if BF.Power.separator then
-                    BF.Power.separator:SetIgnoreParentAlpha(false)
-                end
-                if BF.border then
-                    BF.border:SetFrameLevel(22)
-                end
-            end
+    if not db or not db.enable then return end
+
+    local name = E:StringTitle(group)
+    if name == "Tank" and config.Maintankofftank then
+        for i = 1, 2 do
+            local f = _G["ElvUF_TankUnitButton"..i]
+            BorderAndSeparator(f)
         end
-
-        if unit == "arena"..i and enabled then
-            if E.db.AYIJE.border.Arena and E.db.unitframe.units.arena.enable then
-                for i = 1, 5 do
-                    local AF = _G["ElvUF_Arena"..i]
-                    BorderAndSeparator(AF)
-                end
-            end
-        end
-    end
-end
-
-function S:ElvUI_UnitFramesGroupRaidParty(_, group, groupFilter, template, headerTemplate, skip)
-	local db = UF.db.units[group]
-	local Header = UF[group]
-
-	local enable = db.enable
-	local name, isRaidFrames = E:StringTitle(group), strmatch(group, '^raid(%d)') and true
-
-    if name == "Tank" and enable and E.db.AYIJE.border.Maintankofftank then
-        for i = 1, 2 do 
-            local TankFrame = _G["ElvUF_TankUnitButton"..i]
-            BorderAndSeparator(TankFrame)
-        end
-    end
-
-    if name == "Assist" and enable and E.db.AYIJE.border.AssistUnits then
-        for i = 1, 2 do 
-            local AssistFrame = _G["ElvUF_AssistUnitButton"..i]
-            BorderAndSeparator(AssistFrame)
+    elseif name == "Assist" and config.AssistUnits then
+        for i = 1, 2 do
+            local f = _G["ElvUF_AssistUnitButton"..i]
+            BorderAndSeparator(f)
         end
     end
 end
 
 function S:ElvUI_UnitFrames_PostUpdateAura(uf, _, button)
-    if not E.db.AYIJE.border.AuraUF then
-        return
-    end
-
-    if uf.isNameplate then
-        return
-    end
+    if not E.db.AYIJE or not E.db.AYIJE.border.AuraUF then return end
+    if uf.isNameplate then return end
 
     if not button.IsBorder then
         BORDER:CreateBorder(button)
         BORDER:BindBorderColorWithBorder(button.border, button)
-
         button.IsBorder = true
     end
-    
+
     if not button.lastColorUpdate or GetTime() - button.lastColorUpdate > 0.1 then
         local r, g, b, a = button.border:GetBackdropBorderColor()
         button.Count:SetTextColor(r, g, b, a)
@@ -218,21 +150,15 @@ function S:ElvUI_UnitFrames_PostUpdateAura(uf, _, button)
 end
 
 function S:ElvUI_UnitFrames_Configure_AuraBars(_, f)
-	local auraBars = f.AuraBars
-	local db = f.db
-	if db.aurabar.enable then
-		for _, statusBar in ipairs(auraBars) do
-			S:ElvUI_UnitFrames_Construct_AuraBars(nil, statusBar)
-		end
-	end
+    if not f.db or not f.db.aurabar or not f.db.aurabar.enable then return end
+    for _, statusBar in ipairs(f.AuraBars) do
+        S:ElvUI_UnitFrames_Construct_AuraBars(nil, statusBar)
+    end
 end
 
 function S:ElvUI_UnitFrames_Construct_AuraBars(_, f)
     if not f then return end
-    
     f.icon:SetPoint("RIGHT", f, "LEFT", -9, 0)
-
-    -- Cache processed children to prevent redundant updates
     for _, child in next, { f:GetChildren() } do
         if not child.IsBorder then
             BORDER:CreateBorder(child)
@@ -241,22 +167,51 @@ function S:ElvUI_UnitFrames_Construct_AuraBars(_, f)
     end
 end
 
+function S:ProfileUpdate()
+    local config = E.db.AYIJE and E.db.AYIJE.border
+    if not config then return end
+
+    local units = { "player", "target", "targettarget", "focus", "focustarget", "pet", "pettarget", "targettargettarget" }
+    for _, unit in ipairs(units) do
+        local f = UF[unit]
+        if f and UF.db.units[unit] and UF.db.units[unit].enable then
+            BorderAndSeparator(f)
+        end
+    end
+
+    for i = 1, 5 do
+        local boss = _G["ElvUF_Boss"..i]
+        if boss and config.Boss and E.db.unitframe.units.boss.enable then
+            BorderAndSeparator(boss)
+        end
+        local arena = _G["ElvUF_Arena"..i]
+        if arena and config.Arena and E.db.unitframe.units.arena.enable then
+            BorderAndSeparator(arena)
+        end
+    end
+
+    for i = 1, 2 do
+        local tank = _G["ElvUF_TankUnitButton"..i]
+        if tank and config.Maintankofftank then
+            BorderAndSeparator(tank)
+        end
+        local assist = _G["ElvUF_AssistUnitButton"..i]
+        if assist and config.AssistUnits then
+            BorderAndSeparator(assist)
+        end
+    end
+end
+
 function S:UnitFrames()
-    -- Player, Target, Target of Target, Focus, Focus Target, Pet, Pet Target, Target of Target of Target. 
-	S:SecureHook(UF, "CreateAndUpdateUF", "ElvUI_UnitFrames")
-    
-    -- Boss and Arena.
+    S:SecureHook(UF, "CreateAndUpdateUF", "ElvUI_UnitFrames")
     S:SecureHook(UF, "CreateAndUpdateUFGroup", "ElvUI_UnitFramesGroup")
-
-    -- Party and Raid.
     S:SecureHook(UF, "CreateAndUpdateHeaderGroup", "ElvUI_UnitFramesGroupRaidParty")
-
-    --Aura's on Unitframes. 
     S:SecureHook(UF, "PostUpdateAura", "ElvUI_UnitFrames_PostUpdateAura")
+    S:SecureHook(UF, "Configure_AuraBars", "ElvUI_UnitFrames_Configure_AuraBars")
+    S:SecureHook(UF, "Construct_AuraBars", "ElvUI_UnitFrames_Construct_AuraBars")
 
-    -- Status bar
-	S:SecureHook(UF, "Configure_AuraBars", "ElvUI_UnitFrames_Configure_AuraBars")
-	S:SecureHook(UF, "Construct_AuraBars", "ElvUI_UnitFrames_Construct_AuraBars")
+    -- ✅ Ensure it runs after profile swap
+    hooksecurefunc(E, "StaggeredUpdateAll", function() S:ProfileUpdate() end)
 end
 
 S:AddCallback("UnitFrames")
