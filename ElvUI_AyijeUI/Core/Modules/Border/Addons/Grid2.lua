920b375b27716af79dbc3ae3255399cfdaf6a826
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-09-03 14:53:51 +0200

    Test fix

--- ElvUI_AyijeUI/Core/Modules/Border/Addons/Grid2.lua
+++ ElvUI_AyijeUI/Core/Modules/Border/Addons/Grid2.lua
@@ -11,6 +11,35 @@ if E.db.forceSeparators == nil then
     E.db.forceSeparators = false
 end
 
+local function CleanupIndividualFrameBorders()
+    -- Clean up individual unit frame borders when transitioning to raid
+    for o = 1, 2 do
+        for u = 1, 5 do
+            local unitName = "Grid2LayoutHeader" .. o .. "UnitButton" .. u
+            local unitButton = _G[unitName]
+            
+            if unitButton then
+                -- Remove individual borders
+                if unitButton.border then 
+                    unitButton.border:Hide()
+                    unitButton.border:ClearAllPoints()
+                    unitButton.border:SetParent(nil)
+                    unitButton.border = nil
+                    unitButton.IsBorder = false
+                end
+                
+                -- Remove separators
+                if unitButton.separator then
+                    unitButton.separator:Hide()
+                    unitButton.separator:ClearAllPoints()
+                    unitButton.separator:SetParent(nil)
+                    unitButton.separator = nil
+                end
+            end
+        end
+    end
+end
+
 local function GetUnitFrame()
     local spec = GetSpecialization()
     local specID = GetSpecializationInfo(spec)
@@ -25,6 +54,8 @@ local function GetUnitFrame()
     }
 
     if IsInRaid() then
+        -- Clean up individual frame borders first before creating layout frame border
+        CleanupIndividualFrameBorders()
         BORDER:CreateBorder(_G.Grid2LayoutFrame)
         return
     else
@@ -104,7 +135,6 @@ end
 
 local function Grid2GetUnitFrame()
     GetUnitFrame()
-
 end
 
 function S:Grid2()
@@ -125,64 +155,3 @@ function S:Grid2()
 end
 
 S:AddCallbackForAddon("Grid2")
-
-
--- Old code for safe keeping. 
---[[
-local function GetUnitFrame()
-	if IsInRaid() then return end
-
-	local partyFrames = {
-			LGF.GetUnitFrame("player", { raidFrames = "^Grid2Layout" }),
-			LGF.GetUnitFrame("party1", { raidFrames = "^Grid2Layout" }),
-			LGF.GetUnitFrame("party2", { raidFrames = "^Grid2Layout" }),
-			LGF.GetUnitFrame("party3", { raidFrames = "^Grid2Layout" }),
-			LGF.GetUnitFrame("party4", { raidFrames = "^Grid2Layout" })
-	}
-
-	for _, frame in pairs(partyFrames) do
-			if (frame and _G.Grid2LayoutHeader1UnitButton1 and frame:GetName() == _G.Grid2LayoutHeader1UnitButton1:GetName()) or (frame and _G.Grid2LayoutHeader2UnitButton1 and frame:GetName() == _G.Grid2LayoutHeader2UnitButton1:GetName()) then
-				if frame.separator then
-					frame.separator:Hide()
-				end
-			else
-					if frame then
-							if UnitExists(frame.unit) and not frame.SEPARATOR then
-								BORDER:CreateSeparator(frame)
-								frame.separator:SetPoint("TOPRIGHT", frame, 0, 3)
-								frame.SEPARATOR = true
-							elseif not UnitExists(frame.unit) and frame.SEPARATOR then
-								if frame.separator then
-									frame.separator:Hide()
-									frame.SEPARATOR = nil
-								end
-							end
-					end
-			end
-	end
-end
-
-local function FrameCallback(event, frame, unit)
-    if event == "GETFRAME_REFRESH" or event == "FRAME_UNIT_UPDATE" or event == "FRAME_UNIT_REMOVED" then
-        GetUnitFrame()
-    end
-end
-
-function S:Grid2()
-    if not E.db.AYIJE.skins.grid2 then return end
-
-    BORDER:CreateBorder(_G.Grid2LayoutFrame)
-
-    _G.Grid2LayoutFrame.border:RegisterEvent("PLAYER_ENTERING_WORLD")
-    _G.Grid2LayoutFrame.border:RegisterEvent("GROUP_ROSTER_UPDATE")
-    _G.Grid2LayoutFrame.border:SetScript("OnEvent", function(self, event, ...)
-        GetUnitFrame()
-    end)
-
-    LGF.RegisterCallback(_G.Grid2LayoutFrame, "GETFRAME_REFRESH", FrameCallback)
-    LGF.RegisterCallback(_G.Grid2LayoutFrame, "FRAME_UNIT_UPDATE", FrameCallback)
-    LGF.RegisterCallback(_G.Grid2LayoutFrame, "FRAME_UNIT_REMOVED", FrameCallback)
-end
-
-S:AddCallbackForAddon("Grid2")
-]]
